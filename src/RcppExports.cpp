70aa0ab824f32cb177356d134cddacf27750793c
Daniel Greene <dg333@cam.ac.uk>
2024-02-20 21:40:02 +0000

    version 1.5

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // es_raw
 double es_raw(IntegerVector S, NumericVector r);
 RcppExport SEXP _gsEasy_es_raw(SEXP SSEXP, SEXP rSEXP) {
